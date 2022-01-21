b34d74b5dd359ff4c56e57758456566b9be2e4d3
The Kaktus Yakov <82301654+Yakov-kaktus@users.noreply.github.com>
2022-01-21 17:27:39 +0700

    kaktus1.py

---
+++ kaktus1.py
@@ -0,0 +1,22 @@
+from tkinter import *
+t = Tk()
+c = Canvas(t, width=1000, height=500)
+t.title("go!")
+c.pack()
+
+o = PhotoImage(file="Новый проект.png")
+j = c.create_image(0,0,anchor=NW, image=o)
+pi = PhotoImage(file="sbk.png")
+ig = c.create_image(500,250,anchor=NW,image=pi)
+
+
+def movs(event):
+    if event.keysym == "Up":
+        c.move(ig, 0, -10)
+    elif event.keysym == "Down":
+        c.move(ig, 0, 10)
+    elif event.keysym == "Right":
+        c.move(ig, 10, 0)
+    elif event.keysym == "Left":
+        c.move(ig, -10, 0)
+c.bind_all('<Key>', movs)
