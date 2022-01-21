de31e8d457d9447b9dd8800387afd6377f500b26
The Kaktus Yakov <82301654+Yakov-kaktus@users.noreply.github.com>
2022-01-21 17:38:15 +0700

    Update kaktus1.py

--- kaktus1.py
+++ kaktus1.py
@@ -1,20 +1,20 @@
-from tkinter import *
-t = Tk()
-c = Canvas(t, width=1000, height=500)
-t.title("go!")
-c.pack()
+from tkinter import * #импортируем ткинтер
+t = Tk() #задаём его отрисовку переменной
+c = Canvas(t, width=1000, height=500) #создаём холст
+t.title("go!") # делаем заголовок вкладки
+c.pack() #упаковываем холст
 
-pi = PhotoImage(file="sbk.png")
-ig = c.create_image(500,250,anchor=NW,image=pi)
+pi = PhotoImage(file="sbk.png") #загружаем нашу собаку
+ig = c.create_image(500,250,anchor=NW,image=pi) #делаем её объектом на экране
 
 
-def movs(event):
-    if event.keysym == "Up":
-        c.move(ig, 0, -10)
-    elif event.keysym == "Down":
-        c.move(ig, 0, 10)
-    elif event.keysym == "Right":
-        c.move(ig, 10, 0)
-    elif event.keysym == "Left":
-        c.move(ig, -10, 0)
-c.bind_all('<Key>', movs)
+def movs(event): # задаём переменную и даём ей параметр событий( в конце мы укажем что это события с клавиатуры)
+    if event.keysym == "Up": # при нажатии клавиши вверх
+        c.move(ig, 0, -10)#двигаем вверх
+    elif event.keysym == "Down":#при нажатии клавиши вниз
+        c.move(ig, 0, 10) #двигаем вниз
+    elif event.keysym == "Right": #
+        c.move(ig, 10, 0)         #
+    elif event.keysym == "Left":  #
+        c.move(ig, -10, 0)        # и т д
+c.bind_all('<Key>', movs)# при нажатии на клавишу любую срабатывает функция
